e8552fbcf8e9e614a3a5a70a0c969aaa3af36366
guitarranalon <guitarranalon@gmail.com>
2025-09-27 21:55:54 +0200

    [SMSD-14] trying more things to get the full site indexed

--- astro.config.ts
+++ astro.config.ts
@@ -23,8 +23,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-  site: 'https://guitarranalon.github.io/siamese-dev/',
-  base: '/siamese-dev/',
+  site: 'https://guitarranalon.github.io/siamese-dev',
+  base: '/siamese-dev',
   integrations: [
     tailwind({
       applyBaseStyles: false,
